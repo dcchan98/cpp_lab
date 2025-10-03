38f229cd5a063b950914eba1c67d6df4d748a613
dcchan98 <dcchan98@gmail.com>
2025-10-04 03:19:14 +0800

    flatten trick

--- main.cpp
+++ main.cpp
@@ -32,12 +32,19 @@ using namespace std;
 // Modify code below
 
 int main() {
-    /// useful syntax for recursive lambdas
+    // useful syntax for recursive lambdas
     auto fib = [&](this auto func, int n) -> int {
         if (n==1) {return 0;}
         if (n==2) {return 1;}
         return func(n-1)+func(n-2);
     };
-    print(fib(20));
+    print(fib(5));
+
+    // Flatten 2d vector to 1d -> use formula i*n+j for access
+    int m = 5,  n = 6, i = 2, j = 3;
+    vector<int> dp1d(m*n,-INFINITY);
+    print(dp1d[i*n+j]);
+    // this removes the need for dp2d below
+    vector<vector<int>> dp2d(m,vector<int>(n,-INFINITY));
     return 0;
 }
